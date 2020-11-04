037e4e47ae048f5bb80c4bea89581cc3bd64ca56
Nahin-CDR <dev.nahin95@gmail.com>
2020-11-04 19:05:37 +0600

    First Commit

---
+++ app/src/main/java/com/ncoder/pushnotification/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ncoder.pushnotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+}
